4502003ef4001a8b6edc63a79bba03ce674e012b
quangnvhe172037 <quangnvhe172037@fpt.edu.vn>
2023-11-07 00:21:00 +0700

    update api

--- src/api/baseapi.js
+++ src/api/baseapi.js
@@ -1,2 +1,2 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://quang-nv1911.onrender.com";
 export default BASE_URL;
--- src/api/frontendapi.js
+++ src/api/frontendapi.js
@@ -1,2 +1,2 @@
-const FE_URL = "http://localhost:8081";
+const FE_URL = "https://quangnv1911-fe.onrender.com";
 export default FE_URL;
